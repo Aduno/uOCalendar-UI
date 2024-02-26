b2233a75a2c90a89c988ccd1997f6aaf3c58295c
aduno <aduno7647@gmail.com>
2024-02-25 21:02:19 -0500

    Adding file upload functionality and basic template for reporting bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,30 +4,78 @@
 // @ts-nocheck
 
   let selectedFile;
-
-  function handleFileUpload(event) {
+  function handleDrop(event) {
     selectedFile = event.target.files[0];
-    // Handle the uploaded file here
-    // Check if selectedfile is .htm
-    if (selectedFile.name.split('.').pop() !== 'htm') {
-      alert('Please upload a  file');
-      selectedFile = null;
-    }
+    checkFile(selectedFile);
+    uploadFile(selectedFile);
   }
-
-  function handleDragDrop(event) {
+  function handleInsert(event) {
     preventDefault(event);
     selectedFile = event.dataTransfer.files[0];
-    // Handle the uploaded file here
-    // Check if selectedfile is .htm
-    if (selectedFile.name.split('.').pop() !== 'htm') {
-      alert('Please upload a  file');
-      selectedFile = null;
-    }
+    checkFile(selectedFile);
+    uploadFile(selectedFile);
   }
   
   function preventDefault(event) {
     event.preventDefault();
+  }
+
+  function checkFile(selectedFile) {
+    if (selectedFile.name.split('.').pop() !== 'htm') {
+      alert('Please upload the SA_LEARNER_SERVICES.htm file');
+      selectedFile = null;
+    }
+    else if(selectedFile.size > 150000 || selectedFile.size < 60000) {
+      alert('Please check if you are uploading the correct file SA_LEARNER_SERVICES.htm file');
+      selectedFile = null;
+    }
+  }
+  function uploadFile(selectedFile) {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    fetch('https://uocalendar-a68f83215641.herokuapp.com/uocalendar', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'My Schedule.ics'; 
+      document.body.appendChild(a); 
+      a.click();
+      a.remove();  
+      console.log('Success');
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // if error code is 500s, alert the user the server is down 
+      if (error.status >= 500) {
+        alert('Server is down, please try again later');
+      }
+      else {
+        // Alert the user that there was an error processing the file and let them chose whether to send the 
+        // file to the developer for further investigation
+        if (confirm('There was an error processing the file, would you like to send the file to the developer for further investigation?')) {
+          const formData = new FormData();
+          formData.append('file', selectedFile);
+          fetch('https://uocalendar-a68f83215641.herokuapp.com/errors', {
+            method: 'POST',
+            body: formData
+          })
+          // then show the user a message that the file has been sent to the developer
+          .then(response => response.json())
+          .then(data => {
+            alert(data.message);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error sending the report. Please contact the developer directly through the contact form');
+          });
+        }
+      }
+    });
   }
 </script>
 
@@ -41,9 +89,9 @@
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="upload-box" on:dragover={preventDefault} 
-  on:dragenter={preventDefault} on:drop={handleDragDrop}>
+  on:dragenter={preventDefault} on:drop={handleInsert}>
   <h1>Upload .htm File</h1>
-    <input type="file" accept=".htm" on:change={handleFileUpload} />
+    <input type="file" accept=".htm" on:change={handleDrop} />
   {#if selectedFile}
     <p>Selected File: {selectedFile.name}</p>
   {/if}
