32a4daa669e2ac00b151b120777569f2f4c39cb9
aduno <aduno7647@gmail.com>
2024-03-13 13:26:29 -0400

    Changing website name

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -3,9 +3,14 @@
   import './header.css';
   import geegeeIcon from '$lib/assets/geegee.png'
 </script>
+<style>
+  header {
+    background-color: rgb(101, 29, 50);
+  }
+</style>
 <header>
     <nav class="navbar flex flex-row justify-between">
       <img src={geegeeIcon} alt="GeeGee Icon" class="w-20 h-16 p-3" />
-      <p class="text-3xl text-center font-bold self-center mx-auto text-white ">ICS Generator</p>
+      <p class="text-3xl text-center font-bold self-center mx-auto text-white ">UOCalendar</p>
     </nav>
 </header>
